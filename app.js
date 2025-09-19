612d545d046c49ab0ef660dc0990c215a349592f
GuedesSiilva <davig0400@gmail.com>
2025-09-19 11:10:44 -0300

    Comentário 1.0

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const divResposta = document.getElementById('resposta');
 
 botaoAcao.addEventListener('click', function() {
   if (nome.value.trim() === "") {
-        divResposta.innerHTML = `<p>Por favor, insira seu nome para entrar no mundo mágico!</p>`;
+        divResposta.innerHTML = `<p style = "color: red;" >Por favor, insira seu nome para entrar no mundo mágico!</p>`;
     } else {
     divResposta.innerHTML = `<p>Olá ${nome.value}, Bem-vindo a magia</p>`;
     }
